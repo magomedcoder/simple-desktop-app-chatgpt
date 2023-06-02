9341172581309f0449835e412bc209db3f76fb23
Magomedcoder <info@magomedcoder.ru>
2023-06-02 13:42:32 +0300

    Added creation window

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log('Test')
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+
+const createWindow = (): void => {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 700,
+    title: 'ChatGPT',
+    autoHideMenuBar: true,
+    resizable: false
+  })
+  win.loadFile(path.join(__dirname, '../src/index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
